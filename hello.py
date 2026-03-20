af5e68e78b22e792906c0a77fc1b3fc430f803e0
Jebb Burditt <jebb.burditt@gmail.com>
2026-03-20 10:27:35 -0700

    start of login page

--- hello.py
+++ hello.py
@@ -8,11 +8,20 @@ app = Flask(__name__)
 def index():
     return 'Index Page'
 
+@app.get('/login')
+def login_get():
+    return True
+
+@app.post('/login')
+def login_post():
+    return True
+    
 @app.route("/hello")
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
 
+# parameter types are string, int, float, path, uuid
 @app.route("/user/<int:id>")
 def getUser(id):
     return f"Get user {id}"
