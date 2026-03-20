d7093629a11cc9db551f7a0f2e1bf755453615c7
Jebb Burditt <jebb.burditt@gmail.com>
2026-03-20 10:12:09 -0700

    get user service display parameter from router

--- hello.py
+++ hello.py
@@ -4,7 +4,15 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return 'Index Page'
+
 @app.route("/hello")
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
+
+@app.route("/user/<int:id>")
+def getUser(id):
+    return f"Get user {id}"
